281e9daa713d1b980eaf7a39600ebce773b53c69
Khrystyna Hrekul <grekulkristi@gmail.com>
2022-02-12 16:02:03 +0200

    api Base_URL

---
+++ src/js/api/fetchCountries.js
@@ -0,0 +1,11 @@
+const BASE_URL = `https://restcountries.com/v3.1/name`;
+
+export function fetchCountries(name) {
+    return fetch(`${BASE_URL}/&{name}?fields=name,capital,population,flags,languages`)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+            return res.json();
+        });
+};
